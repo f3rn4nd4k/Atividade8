220738462f14bbc85a6934f372d72562b69c2a93
fks <143506486+f3rn4nd4k@users.noreply.github.com>
2025-10-29 10:45:40 -0300

    Create HomeFragment.kt

---
+++ res/navigation/res/layout/HomeFragment.kt
@@ -0,0 +1,34 @@
+package com.example.animeverse.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.animeverse.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+
+        binding.btnShowDetails.setOnClickListener {
+            val action = HomeFragmentDirections
+                .actionHomeFragmentToDetailsFragment("Naruto Shippuden")
+            findNavController().navigate(action)
+        }
+
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
